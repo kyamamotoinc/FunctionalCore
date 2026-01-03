55b50e2294b63660b4ebe037b9d0623044d7d640
kyamamotoinc <37946191+kyamamotoinc@users.noreply.github.com>
2026-01-03 22:45:53 +0900

    Update Option.cs

--- src/Option.cs
+++ src/Option.cs
@@ -1,158 +1,158 @@
+using System;
+
 namespace FunctionalCore
 {
-    namespace FunctionalCore
+    public sealed class Option<T>
     {
-        public sealed class Option<T>
+        public bool HasValue { get; }
+
+        private readonly T _value;
+        public T Value
         {
-            public bool HasValue { get; }
-
-            private readonly T _value;
-            public T Value
-            {
-                get
-                {
-                    if (HasValue == false)
-                    {
-                        throw new InvalidOperationException("No value available");
-                    }
-                    return _value;
-                }
-            }
-
-            private Option(T value)
-            {
-                HasValue = true;
-                _value = value;
-            }
-
-            private Option()
-            {
-                HasValue = false;
-                _value = default;
-            }
-
-            public static Option<T> Some(T value) => new Option<T>(value);
-
-            public static Option<T> None() => new Option<T>();
-
-            public Option<U> Map<U>(Func<T, U> f)
-            {
-                return HasValue ? Option<U>.Some(f(Value)) : Option<U>.None();
-            }
-
-            public Option<U> Bind<U>(Func<T, Option<U>> f)
-            {
-                return HasValue ? f(Value) : Option<U>.None();
-            }
-
-            #region "For LINQ"
-
-            public Option<U> Select<U>(Func<T, U> f)
-            {
-                return Map(f);
-            }
-
-            public Option<V> SelectMany<U, V>(Func<T, Option<U>> selector, Func<T, U, V> projector)
-            {
-                return Bind(x => selector(x).Map(y => projector(x, y)));
-            }
-            #endregion
-
-            public Option<T> Tap(Action<Option<T>> act)
+            get
             {
                 if (HasValue == false)
                 {
-                    return this;
+                    throw new InvalidOperationException("No value available");
                 }
-                act(this);
+                return _value;
+            }
+        }
+
+        private Option(T value)
+        {
+            HasValue = true;
+            _value = value;
+        }
+
+        private Option()
+        {
+            HasValue = false;
+            _value = default;
+        }
+
+        public static Option<T> Some(T value) => new Option<T>(value);
+
+        public static Option<T> None() => new Option<T>();
+
+        public Option<U> Map<U>(Func<T, U> f)
+        {
+            return HasValue ? Option<U>.Some(f(Value)) : Option<U>.None();
+        }
+
+        public Option<U> Bind<U>(Func<T, Option<U>> f)
+        {
+            return HasValue ? f(Value) : Option<U>.None();
+        }
+
+        #region "For LINQ"
+
+        public Option<U> Select<U>(Func<T, U> f)
+        {
+            return Map(f);
+        }
+
+        public Option<V> SelectMany<U, V>(Func<T, Option<U>> selector, Func<T, U, V> projector)
+        {
+            return Bind(x => selector(x).Map(y => projector(x, y)));
+        }
+        #endregion
+
+        public Option<T> Tap(Action<Option<T>> act)
+        {
+            if (HasValue == false)
+            {
                 return this;
             }
+            act(this);
+            return this;
+        }
 
-            public Option<T> TapError(Action<Option<T>> act)
+        public Option<T> TapError(Action<Option<T>> act)
+        {
+            if (HasValue)
             {
-                if (HasValue)
-                {
-                    return this;
-                }
-                act(this);
                 return this;
             }
+            act(this);
+            return this;
+        }
 
-            public Option<T> TapAll(Action<Option<T>> act)
-            {
-                act(this);
-                return this;
-            }
+        public Option<T> TapAll(Action<Option<T>> act)
+        {
+            act(this);
+            return this;
+        }
 
-            public U Match<U>(Func<T, U> funcOnSuccess, Func<U> funcOnFailure)
-            {
-                return HasValue ? funcOnSuccess(Value) : funcOnFailure();
-            }
+        public U Match<U>(Func<T, U> funcOnSuccess, Func<U> funcOnFailure)
+        {
+            return HasValue ? funcOnSuccess(Value) : funcOnFailure();
+        }
 
-            public Option<U> Combine<R, U>(Option<R> other, Func<T, R, U> f)
+        public Option<U> Combine<R, U>(Option<R> other, Func<T, R, U> f)
+        {
+            if (HasValue == false)
             {
-                if (HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                if (other.HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                return Option<U>.Some(f(Value, other.Value));
+                return Option<U>.None();
             }
+            if (other.HasValue == false)
+            {
+                return Option<U>.None();
+            }
+            return Option<U>.Some(f(Value, other.Value));
+        }
 
-            public Option<U> Combine<R1, R2, U>(Option<R1> other1, Option<R2> other2, Func<T, R1, R2, U> f)
+        public Option<U> Combine<R1, R2, U>(Option<R1> other1, Option<R2> other2, Func<T, R1, R2, U> f)
+        {
+            if (HasValue == false)
             {
-                if (HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                if (other1.HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                if (other2.HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                return Option<U>.Some(f(Value, other1.Value, other2.Value));
+                return Option<U>.None();
             }
+            if (other1.HasValue == false)
+            {
+                return Option<U>.None();
+            }
+            if (other2.HasValue == false)
+            {
+                return Option<U>.None();
+            }
+            return Option<U>.Some(f(Value, other1.Value, other2.Value));
+        }
 
-            public Option<U> Combine<R1, R2, R3, U>(Option<R1> other1, Option<R2> other2, Option<R3> other3, Func<T, R1, R2, R3, U> f)
+        public Option<U> Combine<R1, R2, R3, U>(Option<R1> other1, Option<R2> other2, Option<R3> other3, Func<T, R1, R2, R3, U> f)
+        {
+            if (HasValue == false)
             {
-                if (HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                if (other1.HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                if (other2.HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                if (other3.HasValue == false)
-                {
-                    return Option<U>.None();
-                }
-                return Option<U>.Some(f(Value, other1.Value, other2.Value, other3.Value));
+                return Option<U>.None();
             }
+            if (other1.HasValue == false)
+            {
+                return Option<U>.None();
+            }
+            if (other2.HasValue == false)
+            {
+                return Option<U>.None();
+            }
+            if (other3.HasValue == false)
+            {
+                return Option<U>.None();
+            }
+            return Option<U>.Some(f(Value, other1.Value, other2.Value, other3.Value));
+        }
 
-            public T ValueOrDefault(T defaultValue)
-            {
-                return HasValue ? Value : defaultValue;
-            }
+        public T ValueOrDefault(T defaultValue)
+        {
+            return HasValue ? Value : defaultValue;
+        }
 
-            public T ValueOrThrow(Func<Exception> toException)
+        public T ValueOrThrow(Func<Exception> toException)
+        {
+            if (HasValue)
             {
-                if (HasValue)
-                {
-                    return Value;
-                }
-                throw toException();
+                return Value;
             }
+            throw toException();
         }
     }
 }
+
